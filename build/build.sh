223d3246e2222686151f36619fbee9c6f5bdd7e6
Markus Krogemann <markus@krogemann.de>
2013-08-23 01:09:38 +0200

    bundle update

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,7 @@
 # this script is here to be used by Go and similar CI/CD products. It may at some point be replaced by a Rakefile.
 
 rvm use 1.9.3@golr --create --fuzzy
-bundle
+bundle update
 [ -d rspec ] && rm -rf rspec
 COVERAGE=true bundle update exec rspec --out rspec/rspec.xml --format html --out rspec/rspec.html
 
