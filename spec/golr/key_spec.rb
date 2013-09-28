8baee801f8976ef3b0c60699f67285be72820d4f
Markus Krogemann <markus@krogemann.de>
2013-09-29 01:04:57 +0200

    formatting

--- spec/golr/key_spec.rb
+++ spec/golr/key_spec.rb
@@ -2,21 +2,17 @@ require 'spec_helper'
 require 'golr/key'
 
 module Golr
-
   describe Key do
-
-    context '#coordinates' do
-      it "splits a key given as String into an array of integer coordinates" do
+    describe '#coordinates' do
+      it 'splits a key given as String into an array of integer coordinates' do
         Key.coordinates('1_3').should == [1,3]
       end
     end
 
-    context '#key' do
-      it "concatenates two given values into a key (String)" do
+    describe '#key' do
+      it 'concatenates two given values into a key (String)' do
         Key.key(1,3).should == '1_3'
       end
     end
-
   end
-
 end
