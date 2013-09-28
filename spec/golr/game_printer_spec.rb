048baab8e506fc44b5cf189ed704a8838938ad2b
Markus Krogemann <markus@krogemann.de>
2013-09-29 01:01:32 +0200

    formatting

--- spec/golr/game_printer_spec.rb
+++ spec/golr/game_printer_spec.rb
@@ -2,19 +2,14 @@ require 'spec_helper'
 require 'golr/game_printer'
 
 module Golr
-
   describe GamePrinter do
-
-    context '#print' do
-
+    describe '#print' do
       it 'produces expected output in given io object (String)' do
-        game = Game.new(9,7, ["2_4", "3_5", "1_3", "5_5"])
+        game = Game.new(9, 6, ["2_4", "3_5", "1_3", "5_5"])
         io = StringIO.new
         GamePrinter.print(game, io)
-        io.string.should == "\n|         |\n|         |\n|o        |\n| o       |\n|  o o    |\n|         |\n|         |\n\n"
+        io.string.should == "\n|         |\n|         |\n|o        |\n| o       |\n|  o o    |\n|         |\n\n"
       end
     end
-
   end
-
 end
